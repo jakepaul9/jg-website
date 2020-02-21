3d66c68f6a4a7330899046994501cd50310c9ae0
Jake Grether <jakegrether@Jakes-MacBook-Pro.local>
2020-02-20 19:24:55 -0700

    Updates

---
+++ precache-manifest.722fd3fdf58a302658aa2c6f8517bd4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb417c7d6d80d1af69b6147612b0e69",
+    "url": "/jakepaul9/jg-website.git/index.html"
+  },
+  {
+    "revision": "9758b9232786b08097fb",
+    "url": "/jakepaul9/jg-website.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "308d55d3e7f9c43bda1d",
+    "url": "/jakepaul9/jg-website.git/static/js/2.5eb25044.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jakepaul9/jg-website.git/static/js/2.5eb25044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9758b9232786b08097fb",
+    "url": "/jakepaul9/jg-website.git/static/js/main.86d01bbe.chunk.js"
+  },
+  {
+    "revision": "cad131129a3928394938",
+    "url": "/jakepaul9/jg-website.git/static/js/runtime-main.3173dd5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jakepaul9/jg-website.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
